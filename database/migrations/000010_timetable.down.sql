4ad68385be8c02a14844369d99bd680bfb7855b7
notharsh0905 <harsh0905v@gmail.com>
2026-06-21 14:42:59 +0530

    ...

---
+++ database/migrations/000010_timetable.down.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS faculty_availability CASCADE;
+
+DROP TABLE IF EXISTS timetable_entries CASCADE;
+
+DROP TABLE IF EXISTS timetables CASCADE;
+
+DROP TABLE IF EXISTS working_days CASCADE;
+
+DROP TABLE IF EXISTS time_slots CASCADE;
+
+DROP TYPE IF EXISTS timetable_entry_type;
+
+DROP TYPE IF EXISTS day_of_week;
---
+++ database/migrations/000011_attendance.down.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS attendance_exceptions CASCADE;
+
+DROP TABLE IF EXISTS attendance_records CASCADE;
+
+DROP TABLE IF EXISTS class_sessions CASCADE;
+
+DROP TYPE IF EXISTS session_status;
+
+DROP TYPE IF EXISTS attendance_status;
