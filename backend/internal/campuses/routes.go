dafe8050adbd0806f4f8716deda5b5559fecf029
notharsh0905 <harsh0905v@gmail.com>
2026-06-23 15:23:16 +0530

    routes created successfully

---
+++ backend/internal/campuses/routes.go
@@ -0,0 +1,14 @@
+package campuses
+
+import "github.com/gin-gonic/gin"
+
+func RegisterRoutes(
+	router *gin.RouterGroup,
+	handler *Handler,
+) {
+
+	router.GET(
+		"",
+		handler.ListCampuses,
+	)
+}
