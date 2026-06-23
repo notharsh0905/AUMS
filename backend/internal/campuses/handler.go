c0c57355b6ba72067a64636908859d886619d2d9
notharsh0905 <harsh0905v@gmail.com>
2026-06-23 15:22:26 +0530

    handler for campuses created

---
+++ backend/internal/campuses/handler.go
@@ -0,0 +1,46 @@
+package campuses
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	service *Service
+}
+
+func NewHandler(
+	service *Service,
+) *Handler {
+
+	return &Handler{
+		service: service,
+	}
+}
+
+func (h *Handler) ListCampuses(
+	c *gin.Context,
+) {
+
+	campuses, err := h.service.List(
+		c.Request.Context(),
+	)
+
+	if err != nil {
+
+		c.JSON(
+			http.StatusInternalServerError,
+			gin.H{
+				"error": err.Error(),
+			},
+		)
+
+		return
+	}
+
+	c.JSON(
+		http.StatusOK,
+		campuses,
+	)
+}
