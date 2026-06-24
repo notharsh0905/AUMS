ff1b44e31fdbf5df639b7497748181f8e685e78e
notharsh0905 <harsh0905v@gmail.com>
2026-06-24 15:57:56 +0530

    ...

---
+++ backend/internal/examattempts/routes.go
@@ -0,0 +1,19 @@
+package examattempts
+
+import "github.com/gin-gonic/gin"
+
+func RegisterRoutes(
+	router *gin.RouterGroup,
+	handler *Handler,
+) {
+
+	router.GET(
+		"",
+		handler.List,
+	)
+
+	router.POST(
+		"",
+		handler.Create,
+	)
+}
