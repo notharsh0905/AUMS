45879df8d2da871ee68fd2d15a4e85a2183f694e
notharsh0905 <harsh0905v@gmail.com>
2026-06-24 21:23:50 +0530

    ....

--- backend/internal/courses/service.go
+++ backend/internal/courses/service.go
@@ -76,3 +76,24 @@ func (s *Service) Create(
 		},
 	)
 }
+func (s *Service) ListPaginated(
+	ctx context.Context,
+	limit int32,
+	offset int32,
+) ([]generated.Course, error) {
+
+	return s.repository.ListPaginated(
+		ctx,
+		limit,
+		offset,
+	)
+}
+
+func (s *Service) Count(
+	ctx context.Context,
+) (int64, error) {
+
+	return s.repository.Count(
+		ctx,
+	)
+}
